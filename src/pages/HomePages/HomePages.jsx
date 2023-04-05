c3c7b305ee45ec35c3ba53abd735756c262f2405
Yasin <yasin4911@gmail.com>
2023-04-05 12:20:42 +0600

    .

--- src/pages/HomePages/HomePages.jsx
+++ src/pages/HomePages/HomePages.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
+<<<<<<< Updated upstream
 const HomePages = () => {
   return <div>HomePages</div>;
+=======
+const HomePages = ({ item }) => {
+  return <div></div>;
+>>>>>>> Stashed changes
 };
 
 export default HomePages;
