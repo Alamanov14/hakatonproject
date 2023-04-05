64266ec53502e691d6aff1dc3e3ec9e95de796d8
myusername <shermambetov09@gmail.com>
2023-04-05 12:20:29 +0600

    Registration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import Footer from "./components/footer/Footer";
-import Navbar from "./components/Navbar/Navbar";
-import CartContextProvider from "./context/CartContextProvider";
-import ProductContextProvider from "./context/ProductContextProvaider";
-import MainRoutes from "./routes/MainRoutes";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Footer from './components/footer/Footer';
+import Navbar from './components/Navbar/Navbar';
+import AuthContextProvier from './context/AuthContextProvier';
+import CartContextProvider from './context/CartContextProvider';
+import ProductContextProvider from './context/ProductContextProvaider';
+import MainRoutes from './routes/MainRoutes';
 
 const App = () => {
   return (
     <div>
-      <CartContextProvider>
-        <ProductContextProvider>
-          <BrowserRouter>
-            <Navbar />
-            <MainRoutes />
-            <Footer />
-          </BrowserRouter>
-        </ProductContextProvider>
-      </CartContextProvider>
+      <AuthContextProvier>
+        <CartContextProvider>
+          <ProductContextProvider>
+            <BrowserRouter>
+              <Navbar />
+              <MainRoutes />
+              <Footer />
+            </BrowserRouter>
+          </ProductContextProvider>
+        </CartContextProvider>
+      </AuthContextProvier>
     </div>
   );
 };
--- src/pages/HomePages/HomePages.jsx
+++ src/pages/HomePages/HomePages.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { Button } from '@mui/material';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../HomePages/HomePages.css';
 
 const HomePages = () => {
-  return <div>HomePages</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="Main">
+      <div className="Block1">
+        <h1>Новые поступления в этом сезоне </h1>
+        <h3>
+          Утонченные сочетания и бархатные оттенки - вот то, что вы искали в этом сезоне. Время
+          исследовать.
+        </h3>
+        <button onClick={() => navigate(`/shop`)}>Открыть магазин</button>
+
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/shorts/AIGCeeUpSfE"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen></iframe>
+      </div>
+    </div>
+  );
 };
 
 export default HomePages;
