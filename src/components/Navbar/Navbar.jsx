959b9c32371d08d66111b9fc5f81aa42048082c9
myusername <shermambetov09@gmail.com>
2023-04-05 16:46:31 +0600

    Navbar adaptive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -21,25 +20,6 @@ import { useCart } from '../../context/CartContextProvider';
 import { getCountProductsInCart } from '../../helpers/functions';
 import Search from '../Search/Search';
 import { useAuth } from '../../context/AuthContextProvier';
-=======
-import React, { useEffect, useState } from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import "./Navbar.css";
-import { useNavigate, useSearchParams } from "react-router-dom";
-import { Favorite } from "@mui/icons-material";
-import CallIcon from "@mui/icons-material/Call";
-import LocalMallIcon from "@mui/icons-material/LocalMall";
-import { useProduct } from "../../context/ProductContextProvaider";
-import { Badge } from "@mui/material";
-import { useCart } from "../../context/CartContextProvider";
-import { getCountProductsInCart } from "../../helpers/functions";
-import Search from "../Search/Search";
-import { useAuth } from "../../context/AuthContextProvier";
->>>>>>> c06881a14f4fc5c0785973c7f9ffdf9feca39da8
 
 export default function Navbar() {
   const navigate = useNavigate();
@@ -58,7 +38,7 @@ export default function Navbar() {
   }, [addProductToCart]);
 
   function shopViewer() {
-    navigate("/shop");
+    navigate('/shop');
     setModalActive(true);
     console.log(modalActive);
     getProducts();
@@ -68,53 +48,32 @@ export default function Navbar() {
       <AppBar className="NavbarMain" position="static" id="app_bar">
         <Toolbar
           sx={{
-<<<<<<< HEAD
             display: 'flex',
             justifyContent: 'space-between',
             margin: ' 0 200px',
             color: 'black',
           }}>
           <Box className="NavbarLeft" sx={{ display: 'flex', alignItems: 'center' }}>
-=======
-            display: "flex",
-            justifyContent: "space-between",
-            margin: " 0 200px",
-            color: "black",
-          }}
-        >
-          <Box
-            className="NavbarLeft"
-            sx={{ display: "flex", alignItems: "center" }}
-          >
->>>>>>> c06881a14f4fc5c0785973c7f9ffdf9feca39da8
             <Typography
               component="a"
               href="/"
               marginBottom="-10px"
               marginRight="200px"
-<<<<<<< HEAD
               className="LogoMAin">
-=======
-            >
->>>>>>> c06881a14f4fc5c0785973c7f9ffdf9feca39da8
               <img
                 src="http://womazing.s-host.net/wp-content/uploads/2021/02/logo.svg"
                 alt="image"
                 border="0"
               />
             </Typography>
-            <Typography
-              id="Navbar_Navigation"
-              sx={{ display: "flex", alignItems: "center" }}
-            >
+            <Typography id="Navbar_Navigation" sx={{ display: 'flex', alignItems: 'center' }}>
               <Button
                 variant="h6"
                 component="a"
                 href="/"
                 sx={{
-                  textTransform: "none",
-                }}
-              >
+                  textTransform: 'none',
+                }}>
                 Главная
               </Button>
               <Button
@@ -123,74 +82,62 @@ export default function Navbar() {
                 component="a"
                 onClick={() => shopViewer()}
                 sx={{
-                  textTransform: "none",
-                }}
-              >
+                  textTransform: 'none',
+                }}>
                 Магазин
               </Button>
               <Button
                 className="NavbarButton"
                 variant="h6"
                 component="a"
-                onClick={() => navigate("/about")}
+                onClick={() => navigate('/about')}
                 sx={{
-                  textTransform: "none",
-                }}
-              >
+                  textTransform: 'none',
+                }}>
                 О бренде
               </Button>
               <Button
                 className="NavbarButton"
                 variant="h6"
                 component="a"
-                onClick={() => navigate("/contact")}
+                onClick={() => navigate('/contact')}
                 sx={{
-                  textTransform: "none",
-                }}
-              >
+                  textTransform: 'none',
+                }}>
                 Контакты
               </Button>
             </Typography>
           </Box>
 
-          <Box
-            className="NavbarRight"
-            sx={{ display: "flex", alignItems: "center" }}
-          >
+          <Box className="NavbarRight" sx={{ display: 'flex', alignItems: 'center' }}>
             <Box
               className="Call"
               component="a"
               href="tel:+720(495)20823-54-12"
               sx={{
-                display: "flex",
-                alignItems: "center",
-                marginRight: "50px",
-              }}
-            >
+                display: 'flex',
+                alignItems: 'center',
+                marginRight: '50px',
+              }}>
               <CallIcon />
               <p> +7 (495) 823-54-12</p>
             </Box>
 
             <Badge badgeContent={count} color="primary">
-              <LocalMallIcon onClick={() => navigate("/cart")} />
+              <LocalMallIcon onClick={() => navigate('/cart')} />
             </Badge>
 
-            <Favorite onClick={() => navigate("/favorite")} />
+            <Favorite onClick={() => navigate('/favorite')} />
             {email ? (
-              <Button onClick={handleLogout} sx={{ my: 2, display: "block" }}>
+              <Button onClick={handleLogout} sx={{ my: 2, display: 'block' }}>
                 <Typography id="pages_link">Logout</Typography>
               </Button>
             ) : (
-              <Button
-                onClick={() => navigate("/login")}
-                sx={{ my: 2, display: "block" }}
-              >
+              <Button onClick={() => navigate('/login')} sx={{ my: 2, display: 'block' }}>
                 <Typography id="pages_link">Login</Typography>
               </Button>
             )}
-            {window.location.pathname === "/shop" ? (
-              <Search setActive={setModalActive} />
-            ) : null}
+            {window.location.pathname === '/shop' ? <Search setActive={setModalActive} /> : null}
             {console.log(modalActive)}
           </Box>
         </Toolbar>
